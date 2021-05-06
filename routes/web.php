bc44930032733211c581bc0a5de7506c41fb4b97
saadabdurrazaq <seadclark@gmail.com>
2021-05-06 22:29:22 +0700

    Update web.php
    Fixing bugs

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes Settings
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
